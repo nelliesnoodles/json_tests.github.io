1e0b05a94985be90656f0e043eee2145c24d552f
Nellie Tobey <35578867+nelliesnoodles@users.noreply.github.com>
2019-07-04 22:10:09 -0400

    Update json_test.js

--- json_test.js
+++ json_test.js
@@ -5,9 +5,9 @@
     .then(data => {
         // Work with JSON data here
         var element = document.getElementById("json_data");
-        var name = data[0].name;
-        element.innerHTML = name;
-        console.log(data)
+        var array_1 = JSON.stringify(data)
+        //element.innerHTML = name;
+        console.log(array_1)
     })
     .catch(err => {
         // Do something for an error here
