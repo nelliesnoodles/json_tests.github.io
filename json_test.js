a35c4e79434fbdb820cc1d59d683a10a62127e79
Nellie Tobey <35578867+nelliesnoodles@users.noreply.github.com>
2019-07-04 22:07:27 -0400

    loading initial files to test

---
+++ json_test.js
@@ -0,0 +1,14 @@
+﻿fetch('json1.json')
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        // Work with JSON data here
+        var element = document.getElementById("json_data");
+        var name = data[0].name;
+        element.innerHTML = name;
+        console.log(data)
+    })
+    .catch(err => {
+        // Do something for an error here
+    })
