95a0a3400fadd98c9bc9552b9e9bc379911d7230
Nellie Tobey <35578867+nelliesnoodles@users.noreply.github.com>
2019-07-06 09:25:02 -0400

    Update json_test.js

--- json_test.js
+++ json_test.js
@@ -10,11 +10,11 @@
             //var dict = data[i];  //<--  not working
             var element = document.getElementById("json_data");
             element.innerHTML = "fetching...."
-            var dict_set_i = data[0];
+            var dict_set_i = data[i];
             console.log("trial 2, change dict_set_i.get(Name) to dict_set_i.Name; log=");
             console.log(dict_set_i);
             var name = dict_set_i.Name;
-            //element.innerHTML = name;
+            element.innerHTML = name;
             console.log(name);
         })
         .catch(err => {
