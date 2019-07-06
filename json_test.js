6a8f314be8baaacbcbf7653ce685243a60a956d4
Nellie Tobey <35578867+nelliesnoodles@users.noreply.github.com>
2019-07-06 03:02:40 -0400

    testing failed on quiz  testing on fetch

--- json_test.js
+++ json_test.js
@@ -1,15 +1,23 @@
-﻿fetch('json1.json')
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        // Work with JSON data here
-        var element = document.getElementById("json_data");
-        var dict = data[0];
-        var name = dict.get(Name);
-        element.innerHTML = name;
-        console.log(name);
-    })
-    .catch(err => {
-        // Do something for an error here
-    })
+﻿function set_variables(i) {
+
+    fetch('json1.json')
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            // Work with JSON data here
+            var element = document.getElementById("json_data");
+            var dict = data[i];
+            var name = dict.get(Name);
+            element.innerHTML = name;
+            console.log(name);
+        })
+        .catch(err => {
+            // Do something for an error here
+        })
+};
+
+function run_fetch() {
+    var i = 0;
+    set_variables(i);
+}
